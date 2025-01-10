b1029519a37c0be227a60b0708c219cd5d86fc36
Tom Wegener <tom.wegener@allegro-packets.com>
2025-01-10 13:34:49 +0100

    add local carbon-charts example

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { CatalogComponent } from './pages/catalog/catalog.component';
 import { DocsComponent } from './pages/docs/docs.component';
 import { SupportComponent } from './pages/support/support.component';
 import { Link1Component } from './pages/link1/link1.component';
-//import { ChartComponent } from './pages/charts/charts.component';
 import { StarterHomeComponent } from './starter-home/starter-home/starter-home.component';
+import { ChartComponent } from './pages/charts/charts.component';
 
 export const routes: Routes = [
 	{
@@ -18,6 +18,10 @@ export const routes: Routes = [
 	{
 		path: 'docs',
 		component: DocsComponent
+	},
+	{
+		path: 'charts',
+		component: ChartComponent
 	},
 	{
 		path: 'support',
